7de258e98251fcacd35052f04ea6028667e555c3
Petar Iliev <peshote@gmail.com>
2023-07-18 21:22:00 +0300

    Upload new problem solved
    P01. Student Information

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(A)Lab/P01. Student Information/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P01._Student_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double avarageGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {avarageGrade:f2}");
+        }
+    }
+}
