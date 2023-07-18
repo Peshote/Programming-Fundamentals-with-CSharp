e81d2e9d4e02d729a146d0e435ac8f817c91b143
Petar Iliev <peshote@gmail.com>
2023-07-18 21:22:46 +0300

    Upload new problem solved
    P02. Passed

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(A)Lab/P02. Passed/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P02._Passed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var grade = double.Parse(Console.ReadLine());
+            if (grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
