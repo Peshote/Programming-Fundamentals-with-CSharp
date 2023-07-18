b5922a7e9d3588d19d9bf370795befac321d0c5f
Petar Iliev <peshote@gmail.com>
2023-07-18 21:26:07 +0300

    Upload new problem solved
    P11. Multiplication Table 2.0

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(A)Lab/P11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P10._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int theInteger = int.Parse(Console.ReadLine());
+            int multiplicationTimes = int.Parse(Console.ReadLine());
+            int product = 0;
+
+            if (multiplicationTimes > 10)
+            {
+                product = theInteger * multiplicationTimes;
+                Console.WriteLine($"{theInteger} X {multiplicationTimes} = {product}");
+            }
+            while (multiplicationTimes <= 10)
+            {
+                product = theInteger * multiplicationTimes;
+                Console.WriteLine($"{theInteger} X {multiplicationTimes} = {product}");
+                multiplicationTimes++;
+            }            
+        }
+    }
+}
