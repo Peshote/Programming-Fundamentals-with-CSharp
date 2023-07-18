d6dbcc700500ab856615dde37cc64aa559545c60
Petar Iliev <peshote@gmail.com>
2023-07-18 21:26:28 +0300

    Upload new problem solved
    P12. Even Number

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(A)Lab/P12. Even Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P12._Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number is: {Math.Abs(number)}");                
+            }            
+            while (number % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                number = double.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(number)}");
+                    break;
+                }
+            }            
+        }
+    }
+}
