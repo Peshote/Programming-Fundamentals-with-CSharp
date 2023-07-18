308e5270c660b55f330b421061d8eb181653d24e
Petar Iliev <peshote@gmail.com>
2023-07-18 21:25:33 +0300

    Upload new problem solved
    P09. Sum of Odd Numbers

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(A)Lab/P09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P09._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for (var i = 1; i <= n; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                }
+                else
+                {
+                    n++;
+                }
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
