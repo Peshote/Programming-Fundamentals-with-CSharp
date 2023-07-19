e9cda6b729a118c8a0265780587ee640034cc305
Petar Iliev <peshote@gmail.com>
2023-07-19 23:02:49 +0300

    Upload new problem solved
    P06. Strong number

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(B)Exercise/P06. Strong number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P06._Strong_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            string numberString = inputNumber.ToString();
+            int sum = 0;
+
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                int digit = int.Parse(numberString[i].ToString());
+                int factorial = 1;
+
+                for (int j = 2; j <= digit; j++)
+                {
+                    factorial *= j;
+                }
+                sum += factorial;
+            }
+
+            if (sum == inputNumber)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
