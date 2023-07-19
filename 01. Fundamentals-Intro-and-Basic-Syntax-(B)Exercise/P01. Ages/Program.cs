01c37d0b319f9135da1af5921dba47f48202c03a
Petar Iliev <peshote@gmail.com>
2023-07-19 23:01:24 +0300

    Upload new problem solved
    P01. Ages

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(B)Exercise/P01. Ages/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P01._Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string personAge = "";
+            int ageRange = int.Parse(Console.ReadLine());
+
+            if (ageRange >= 0 && ageRange <= 2)
+            {
+                personAge = "baby";
+            }
+            else if (ageRange >= 3 && ageRange <= 13)
+            {
+                personAge = "child";
+            }
+            else if (ageRange >= 14 && ageRange <= 19)
+            {
+                personAge = "teenager";
+            }
+            else if (ageRange >= 20 && ageRange <= 65)
+            {
+                personAge = "adult";
+            }
+            else
+            {
+                personAge = "elder";
+            }
+            Console.WriteLine(personAge);
+        }
+    }
+}
