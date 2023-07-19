8f06053cf1f3a9a299d5fe3958e3f92e9c28017a
Petar Iliev <peshote@gmail.com>
2023-07-19 23:04:19 +0300

    Upload new problem solved
    P11. Orders

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(B)Exercise/P11. Orders/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P11._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ordersCount = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+
+            for (int i = 0; i < ordersCount; i++)
+            {
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int daysInMonth = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                double price = ((daysInMonth * capsulesCount) * pricePerCapsule);
+                totalPrice += price;
+
+                Console.WriteLine($"The price for the coffee is: ${price:F2}");
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+        }
+    }
+}
