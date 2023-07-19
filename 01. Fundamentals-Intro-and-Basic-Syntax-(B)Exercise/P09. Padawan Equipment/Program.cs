265bcd0b8cc7509cd992d598862184c21c1aaa71
Petar Iliev <peshote@gmail.com>
2023-07-19 23:03:43 +0300

    Upload new problem solved
    P09. Padawan Equipment

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(B)Exercise/P09. Padawan Equipment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P09._Padawan_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyAmount = double.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            double pricePerSaber = double.Parse(Console.ReadLine());
+            double pricePerRobe = double.Parse(Console.ReadLine());
+            double pricePerBelt = double.Parse(Console.ReadLine());
+            
+            double lightSabersCount = Math.Ceiling(studentsCount * 0.10);
+            int freeBelts = studentsCount / 6;
+
+            double sabersPrice = (studentsCount + lightSabersCount) * pricePerSaber;
+            double robesPrice = studentsCount * pricePerRobe;
+            double beltsPrice = (studentsCount - freeBelts) * pricePerBelt;
+            double totalSum = sabersPrice + robesPrice + beltsPrice;
+
+            if (totalSum <= moneyAmount)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalSum:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($" John will need {totalSum - moneyAmount:F2}lv more.");
+            }
+        }
+    }
+}
