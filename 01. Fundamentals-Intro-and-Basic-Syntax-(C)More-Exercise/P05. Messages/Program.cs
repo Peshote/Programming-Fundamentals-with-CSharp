4db6920c62e02ce75af9ee32b5f978332c1acfad
Petar Iliev <peshote@gmail.com>
2023-07-20 23:05:43 +0300

    Upload new problem solved
    P05. Messages

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(C)More-Exercise/P05. Messages/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace P05._Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+            string sms = "";
+            
+            for (int i = 0; i < inputCount; i++)
+            {
+                string number = Console.ReadLine();
+
+                int mainDigit = number[0] - '0';
+                
+                if (mainDigit == 0)
+                {
+                    sms += " ";
+                    continue;
+                }
+                int offset = (mainDigit - 2) * 3;
+
+                if (mainDigit == 8 || mainDigit == 9)
+                {
+                    offset += 1;
+                }
+
+                int digitLength = number.Length;
+                int letterIndex = (offset + digitLength -1);
+               
+                char letter = (char)(97 + letterIndex);
+
+                sms += letter;
+            }
+            Console.WriteLine(sms);
+        }
+    }
+}
