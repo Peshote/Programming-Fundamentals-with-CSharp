3ee7479980b9a989c259975cb042c5a7d826b0af
Petar Iliev <peshote@gmail.com>
2023-07-20 23:02:22 +0300

    Upload new problem solved
    P01. Sort Numbers

---
+++ 01. Fundamentals-Intro-and-Basic-Syntax-(C)More-Exercise/P01. Sort Numbers/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace P01._Sort_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = double.Parse(Console.ReadLine());
+            double number2 = double.Parse(Console.ReadLine());
+            double number3 = double.Parse(Console.ReadLine());
+
+            double firstNumber;
+            double secondNumber;
+            double thirdNumber;
+
+            if (number1 >= number2 && number1 >= number3)
+            {
+                firstNumber = number1;
+
+                if (number2 >= number3)
+                {
+                    secondNumber = number2;
+                    thirdNumber = number3;
+                }
+                else
+                {
+                    secondNumber = number3;
+                    thirdNumber = number2;
+                }
+            }
+            else if (number2 >= number1 && number2 >= number3)
+            {
+                firstNumber = number2;
+
+                if (number1 >= number3)
+                {
+                    secondNumber = number1;
+                    thirdNumber = number3;
+                }
+                else
+                {
+                    secondNumber = number3;
+                    thirdNumber = number1;
+                }
+            }
+            else
+            {
+                firstNumber = number3;
+
+                if (number1 >= number2)
+                {
+                    secondNumber = number1;
+                    thirdNumber = number2;
+                }
+                else
+                {
+                    secondNumber = number2;
+                    thirdNumber = number1;
+                }
+            }
+
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+            Console.WriteLine(thirdNumber);
+        }
+    }
+}
