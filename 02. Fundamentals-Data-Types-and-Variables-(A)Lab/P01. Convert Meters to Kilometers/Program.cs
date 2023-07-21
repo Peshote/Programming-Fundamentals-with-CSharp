82463990daf099e6a1bc0ce93da0d75616f57900
Petar Iliev <peshote@gmail.com>
2023-07-21 20:42:50 +0300

    Upload new problem solved
    P01. Convert Meters to Kilometers

---
+++ 02. Fundamentals-Data-Types-and-Variables-(A)Lab/P01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P01._Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int metersDistance = int.Parse(Console.ReadLine());
+            double kilometersDistance = (double)metersDistance / 1000;
+
+            Console.WriteLine($"{kilometersDistance:F2}");
+        }
+    }
+}
