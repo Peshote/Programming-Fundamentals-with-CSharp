6f4abf6b5c6d520a109935d86d1a436569c52d95
Petar Iliev <peshote@gmail.com>
2023-07-21 20:44:41 +0300

    Upload new problem solved
    P09. Chars to String

---
+++ 02. Fundamentals-Data-Types-and-Variables-(A)Lab/P09. Chars to String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P09._Chars_to_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstInput = char.Parse(Console.ReadLine());
+            char secondInput = char.Parse(Console.ReadLine());
+            char thirdInput = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstInput}{secondInput}{thirdInput}");
+        }
+    }
+}
