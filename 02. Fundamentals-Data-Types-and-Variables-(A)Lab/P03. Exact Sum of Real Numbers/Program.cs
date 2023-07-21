93d7a0b6d481b5048fa2d02ea624e02d1e3fd8aa
Petar Iliev <peshote@gmail.com>
2023-07-21 20:43:16 +0300

    Upload new problem solved
    P03. Exact Sum of Real Numbers

---
+++ 02. Fundamentals-Data-Types-and-Variables-(A)Lab/P03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P03._Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            decimal numbersSum = 0m;
+
+            for (int i = 1; i <= numbersCount; i++)
+            {
+                decimal inputNum = decimal.Parse(Console.ReadLine());
+                numbersSum += inputNum;
+            }
+            Console.WriteLine(numbersSum);
+        }
+    }
+}
