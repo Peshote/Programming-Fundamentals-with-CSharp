0a0bfa0decb76735a89047243896c0cbc90d4bfa
Petar Iliev <peshote@gmail.com>
2023-07-22 22:26:24 +0300

    Upload new problem solved
    P09. Spice Must Flow

---
+++ 02. Fundamentals-Data-Types-and-Variables-(B)Exercise/P09. Spice Must Flow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P09._Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int totalSpiceAmount = 0;
+            int yieldDaysCount = 0;
+            while (startingYield >= 100)
+            {
+                totalSpiceAmount += startingYield;
+                startingYield -= 10;
+                totalSpiceAmount -= 26;
+                yieldDaysCount++;
+            }
+
+            totalSpiceAmount -= 26;
+            if (totalSpiceAmount < 0)
+            {
+                totalSpiceAmount = 0;
+            }
+            Console.WriteLine(yieldDaysCount);
+            Console.WriteLine(totalSpiceAmount);
+        }
+    }
+}
