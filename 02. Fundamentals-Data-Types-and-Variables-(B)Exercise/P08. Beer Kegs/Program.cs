8968070c805747e6d3f415222d631ffeff4c9c0d
Petar Iliev <peshote@gmail.com>
2023-07-22 22:26:04 +0300

    Upload new problem solved
    P08. Beer Kegs

---
+++ 02. Fundamentals-Data-Types-and-Variables-(B)Exercise/P08. Beer Kegs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+            string biggestKeg = "";
+            double kegVolume = 0;
+
+            for (int i = 1; i <= inputCount; i++)
+            {
+                string kegModel = Console.ReadLine();
+                double kegRadius = double.Parse(Console.ReadLine());
+                int kegHeight = int.Parse(Console.ReadLine());
+                double currentKegVolume = Math.PI * Math.Pow(kegRadius, 2) * kegHeight;
+
+                if (currentKegVolume > kegVolume)
+                {
+                    kegVolume = currentKegVolume;
+                    biggestKeg = kegModel;
+                }
+            }
+
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
