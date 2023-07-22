d2ea369f920a332d3b4cc8e91c3e75f9aadc891f
Petar Iliev <peshote@gmail.com>
2023-07-22 22:25:43 +0300

    Upload new problem solved
    P07. Water Overflow

---
+++ 02. Fundamentals-Data-Types-and-Variables-(B)Exercise/P07. Water Overflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+            int waterTankCapacity = 255;
+            int waterLitersInTank = 0;
+
+            for (int i = 1; i <= inputCount; i++)
+            {
+                int waterPourQuan = int.Parse(Console.ReadLine());
+                waterLitersInTank += waterPourQuan;
+
+                if (waterLitersInTank > waterTankCapacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    waterLitersInTank -= waterPourQuan;
+                }
+            }
+
+            Console.WriteLine(waterLitersInTank);
+        }
+    }
+}
