10d493f4065fc71f36ae32db0bccb9ed53e65413
Petar Iliev <peshote@gmail.com>
2023-07-22 22:27:22 +0300

    Upload new problem solved
    P11. Snowballs

---
+++ 02. Fundamentals-Data-Types-and-Variables-(B)Exercise/P11. Snowballs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace P11._Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowballsCount = int.Parse(Console.ReadLine());
+            int snowballSnow = 0;
+            int snowballTime = 0;
+            int snowballQuality = 0;
+            BigInteger snowballValue = 0;
+
+            for (int i = 1; i <= snowballsCount; i++)
+            {
+                int snow = int.Parse(Console.ReadLine());
+                int time = int.Parse(Console.ReadLine());
+                int quality = int.Parse(Console.ReadLine());
+                BigInteger currentSnowballValue = BigInteger.Pow(snow / time, quality);
+
+                if (currentSnowballValue > snowballValue)
+                {
+                    snowballSnow = snow;
+                    snowballTime = time;
+                    snowballQuality = quality;
+                    snowballValue = currentSnowballValue;
+                }
+            }
+
+            Console.WriteLine($"{snowballSnow} : {snowballTime} = {snowballValue} ({snowballQuality})");
+        }
+    }
+}
