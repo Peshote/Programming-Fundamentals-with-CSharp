89650ec9250311238ed0879b02cae36fc39e36d6
Petar Iliev <peshote@gmail.com>
2023-07-22 22:26:55 +0300

    Upload new problem solved
    P10. Poke Mon

---
+++ 02. Fundamentals-Data-Types-and-Variables-(B)Exercise/P10. Poke Mon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P10._Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokemonPower = int.Parse(Console.ReadLine());
+            int targetDistance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            double remainingPower = pokemonPower * 0.5d;
+            int targetCount = 0;
+
+            while (pokemonPower >= targetDistance)
+            {
+                pokemonPower -= targetDistance;
+                targetCount++;
+                if (remainingPower == pokemonPower && exhaustionFactor != 0)
+                {
+                    pokemonPower /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(pokemonPower);
+            Console.WriteLine(targetCount);
+        }
+    }
+}
