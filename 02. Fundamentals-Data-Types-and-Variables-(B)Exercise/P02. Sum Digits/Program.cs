047e4d8c695d3b6f98e8ce0b23a0de51743bd2d7
Petar Iliev <peshote@gmail.com>
2023-07-22 22:22:08 +0300

    Upload new problem solved
    P02. Sum Digits

---
+++ 02. Fundamentals-Data-Types-and-Variables-(B)Exercise/P02. Sum Digits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P02._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            string numberString = inputNum.ToString();
+            int numLenght = numberString.Length;
+            int result = 0;
+
+
+            for (int i = 1; i <= numLenght; i++)
+            {
+                char digitChar = numberString[numLenght - i];
+                int currentDigit = int.Parse(digitChar.ToString());
+                result += currentDigit;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
