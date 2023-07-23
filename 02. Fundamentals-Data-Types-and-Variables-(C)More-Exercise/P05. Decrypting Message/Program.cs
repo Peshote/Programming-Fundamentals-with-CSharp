52be75d9ef8d4e49d7cc7c1cc08090524fd17edd
Petar Iliev <peshote@gmail.com>
2023-07-23 21:57:23 +0300

    Upload new problem solved
    P05. Decrypting Message

---
+++ 02. Fundamentals-Data-Types-and-Variables-(C)More-Exercise/P05. Decrypting Message/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P05._Decrypting_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string decryptedMessage = String.Empty;
+            int symbolSum = 0;
+ 
+            for (int i = 0; i < n; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+ 
+                symbolSum = (int)symbol + key;
+                decryptedMessage += (char)symbolSum;
+ 
+            }
+            Console.Write(decryptedMessage);
+        }
+    }
+}
