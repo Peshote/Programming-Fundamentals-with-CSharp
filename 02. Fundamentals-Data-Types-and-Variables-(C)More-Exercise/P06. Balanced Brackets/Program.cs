c97a9d2f17b78b64c1e3b208f3c40e3b8e93cdf5
Petar Iliev <peshote@gmail.com>
2023-07-23 21:57:40 +0300

    Upload new problem solved
    P06. Balanced Brackets

---
+++ 02. Fundamentals-Data-Types-and-Variables-(C)More-Exercise/P06. Balanced Brackets/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P06._Balanced_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = 0; i < inputCount; i++)
+            {
+                string input = Console.ReadLine();
+                
+                if (input == "(")
+                {
+                   counter++;
+                }
+                else if (input == ")")
+                {
+                   counter--;
+                }
+
+                if (counter < 0 || counter > 1)
+                {
+                    break;
+                }
+            }
+
+            if (counter == 0)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
