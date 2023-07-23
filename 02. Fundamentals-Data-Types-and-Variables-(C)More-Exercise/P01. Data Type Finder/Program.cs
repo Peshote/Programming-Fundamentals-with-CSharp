66992667e9d9dc302bdf415e031582135df30913
Petar Iliev <peshote@gmail.com>
2023-07-23 21:56:16 +0300

    Upload new problem solved
    P01. Data Type Finder

---
+++ 02. Fundamentals-Data-Types-and-Variables-(C)More-Exercise/P01. Data Type Finder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P01._Data_Type_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (int.TryParse(input, out var intValue))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (double.TryParse(input, out var doubleValue))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (bool.TryParse(input, out var boolValue))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else if (char.TryParse(input, out var charValue))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
