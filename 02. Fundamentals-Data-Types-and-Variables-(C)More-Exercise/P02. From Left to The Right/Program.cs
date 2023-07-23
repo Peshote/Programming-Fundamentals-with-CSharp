2fe674f7d3542b889c8014ff1a97d9ae0b718040
Petar Iliev <peshote@gmail.com>
2023-07-23 21:56:37 +0300

    Upload new problem solved
    P02. From Left to The Right

---
+++ 02. Fundamentals-Data-Types-and-Variables-(C)More-Exercise/P02. From Left to The Right/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P02._From_Left_to_The_Right
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                string numbersString = Console.ReadLine();
+
+                string[] numbers = numbersString.Split(' ');
+
+                long number1 = long.Parse(numbers[0]);
+                long number2 = long.Parse(numbers[1]);
+
+                long biggerNumber = Math.Max(number1, number2);
+
+                char[] charArray = Math.Abs(biggerNumber).ToString().ToCharArray();
+                
+                int sumOfDigits = 0;
+
+                for (int j = 0; j < charArray.Length; j++)
+                {
+                    sumOfDigits += int.Parse(charArray[j].ToString());
+                }
+                
+                Console.WriteLine(sumOfDigits);
+            }
+        }
+    }
+}
