9fe4e7f221b63920589ff738e9a9ef249e1a3a6f
Petar Iliev <peshote@gmail.com>
2023-07-23 21:56:53 +0300

    Upload new problem solved
    P03. Floating Equality

---
+++ 02. Fundamentals-Data-Types-and-Variables-(C)More-Exercise/P03. Floating Equality/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P03._Floating_Equality
+{
+    internal class Program
+    {
+        public static bool CompareDoubles(double a, double b, double eps = 0.000001)
+        {
+            return Math.Abs(a - b) < eps;
+        }
+        static void Main(string[] args)
+        {
+            double numA = double.Parse(Console.ReadLine());
+            double numB = double.Parse(Console.ReadLine());
+           
+            bool result = CompareDoubles(numA, numB);
+            string output = result ? "True" : "False";
+            output = char.ToUpper(output[0]) + output.Substring(1);
+
+            Console.WriteLine(output);
+        }
+    }
+}
