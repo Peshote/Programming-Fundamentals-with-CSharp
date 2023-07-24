da3f40f3ca6fdbb3e3019548d24ef231b0c325e2
Petar Iliev <peshote@gmail.com>
2023-07-24 22:26:50 +0300

    Upload new problem solved
    P08. Condense Array to Number

---
+++ 03. Fundamentals-Arrays-(A)Lab/P08. Condense Array to Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace P08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] condensedNumbers = new int[numbers.Length - 1];
+            while (numbers.Length > 1)
+            {
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    condensedNumbers[i] = numbers[i] + numbers[i + 1];
+                    if (i == condensedNumbers.Length - 1)
+                    {
+                        numbers = condensedNumbers;
+                        condensedNumbers = new int[numbers.Length - 1];
+                    }
+                }
+            }
+
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
