cb3e5792e6f27f79148703fc72b5cdd850c2fbf0
Petar Iliev <peshote@gmail.com>
2023-07-24 22:25:10 +0300

    Upload new problem solved
    P01. Day of Week

---
+++ 03. Fundamentals-Arrays-(A)Lab/P01. Day of Week/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Dynamic;
+using System.Net;
+
+namespace P01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = 
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+            int day = int.Parse(Console.ReadLine());
+
+            if (day >= 1 && day <=7)
+            {
+                Console.Write(days[day -1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
