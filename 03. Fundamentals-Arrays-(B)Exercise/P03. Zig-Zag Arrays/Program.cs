96143c337f4b49814035801e2851fbeff5228597
Petar Iliev <peshote@gmail.com>
2023-07-25 23:00:32 +0300

    Upload new problem solved
    P03. Zig-Zag Arrays

---
+++ 03. Fundamentals-Arrays-(B)Exercise/P03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq;
+
+namespace P03._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+            int[] arrayNum1 = new int[inputCount];
+            int[] arrayNum2 = new int[inputCount];
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (i % 2 == 0)
+                {
+                    arrayNum1[i] = input[0];
+                    arrayNum2[i] = input[1];
+                }
+                else
+                {
+                    arrayNum1[i] = input[1];
+                    arrayNum2[i] = input[0];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", arrayNum1));
+            Console.WriteLine(string.Join(" ", arrayNum2));
+        }
+    }
+}
