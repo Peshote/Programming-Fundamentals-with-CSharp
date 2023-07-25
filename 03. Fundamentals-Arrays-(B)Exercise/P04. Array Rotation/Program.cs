41babf0696908c8799ccf0b7fb9d94013e77a2e8
Petar Iliev <peshote@gmail.com>
2023-07-25 23:00:49 +0300

    Upload new problem solved
    P04. Array Rotation

---
+++ 03. Fundamentals-Arrays-(B)Exercise/P04. Array Rotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace P04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int rotationsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotationsCount; i++)
+            {
+                int firstElement = numbersArray[0];
+                Array.Copy(numbersArray, 1, numbersArray, 0, numbersArray.Length -1);
+                numbersArray[numbersArray.Length - 1] = firstElement;
+            }
+
+            Console.WriteLine(string.Join(" ", numbersArray));
+        }
+    }
+}
