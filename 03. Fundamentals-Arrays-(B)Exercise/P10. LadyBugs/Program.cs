95986fd44a356b9ea97da9c65fa49c17f5a4798f
Petar Iliev <peshote@gmail.com>
2023-07-25 23:02:36 +0300

    Upload new problem solved
    P10. LadyBugs

---
+++ 03. Fundamentals-Arrays-(B)Exercise/P10. LadyBugs/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace P10._LadyBugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] fieldCells = new int[fieldSize];
+            int[] initialBugsLocation = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < initialBugsLocation.Length; i++)
+            {
+                if (initialBugsLocation[i] >= 0 && initialBugsLocation[i] < fieldSize)
+                {
+                    fieldCells[initialBugsLocation[i]] = 1;
+                }
+            }
+
+            string commands = Console.ReadLine();
+
+            while (commands != "end")
+            {
+                string[] commandsArray = commands
+                    .Split()
+                    .ToArray();
+
+
+                int ladyBugIndex = int.Parse(commandsArray[0]);
+                bool directionRigh = commandsArray[1] == "right";
+                int flyLength = int.Parse(commandsArray[2]);
+
+                if (ladyBugIndex < 0 || ladyBugIndex >= fieldSize || fieldCells[ladyBugIndex] == 0 || flyLength == 0)
+                {
+                    commands = Console.ReadLine();
+                    continue;
+                }
+
+                int newLadyBugLocation = ladyBugIndex;
+
+                if (directionRigh)
+                {
+                    newLadyBugLocation += flyLength;
+                }
+                else
+                {
+                    newLadyBugLocation -= flyLength;
+                }
+
+                while (true)
+                {
+                    if (newLadyBugLocation < 0 || newLadyBugLocation >= fieldSize)
+                    {
+                        fieldCells[ladyBugIndex] = 0;
+                        break;
+                    }
+
+                    if (fieldCells[newLadyBugLocation] == 0)
+                    {
+                        fieldCells[newLadyBugLocation] = 1;
+                        fieldCells[ladyBugIndex] = 0;
+                        break;
+                    }
+
+                    if (directionRigh)
+                    {
+                        newLadyBugLocation += flyLength;
+                    }
+                    else
+                    {
+                        newLadyBugLocation -= flyLength;
+                    }
+                }
+
+                commands = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", fieldCells));
+            int self = 0;
+        }
+    }
+}
