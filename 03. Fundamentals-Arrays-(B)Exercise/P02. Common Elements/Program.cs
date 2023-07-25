520175a6f4c795b76f1bb26675764921a254ecf8
Petar Iliev <peshote@gmail.com>
2023-07-25 22:55:33 +0300

    Upload new problem solved
    P02. Common Elements

---
+++ 03. Fundamentals-Arrays-(B)Exercise/P02. Common Elements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array1 = Console.ReadLine().Split(' ').ToArray();
+            var array2 = Console.ReadLine().Split(' ').ToArray();
+
+            var commonElements = array2.Intersect(array1);
+            string commonElementsString = string.Join(" ", commonElements);
+
+            Console.WriteLine(commonElementsString);
+        }
+    }
+}
