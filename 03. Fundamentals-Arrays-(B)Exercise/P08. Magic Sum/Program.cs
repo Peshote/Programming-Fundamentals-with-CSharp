de93a0c13b7940f5fd12d16dd3570e38cbb84ecb
Petar Iliev <peshote@gmail.com>
2023-07-25 23:02:04 +0300

    Upload new problem solved
    P08. Magic Sum

---
+++ 03. Fundamentals-Arrays-(B)Exercise/P08. Magic Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace P08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                for (int k = 0; k < numbersArray.Length; k++)
+                {
+                    if (k == i)
+                    {
+                        continue;
+                    }
+                    if (numbersArray[i] + numbersArray[k] == inputNumber && i < k)
+                    {
+                        Console.WriteLine($"{numbersArray[i]} {numbersArray[k]}");
+                    }
+                }
+            }
+        }
+    }
+}
