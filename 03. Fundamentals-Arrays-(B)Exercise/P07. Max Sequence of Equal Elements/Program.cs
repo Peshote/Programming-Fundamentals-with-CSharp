f3f7d5de5761b6e0f6b18e2f3b3666d10d811b97
Petar Iliev <peshote@gmail.com>
2023-07-25 23:01:47 +0300

    Upload new problem solved
    P07. Max Sequence of Equal Elements

---
+++ 03. Fundamentals-Arrays-(B)Exercise/P07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace P07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            string longestSequence = "";
+
+            int startIndex = 0;
+            int maxLength = 1;
+            int currentStartIndex = 0;
+            int currentLength = 1;
+
+            for (int i = 1; i < numbersArray.Length; i++)
+            {
+                if (numbersArray[i] == numbersArray[i - 1])
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    currentStartIndex = i;
+                    currentLength = 1;
+                }
+
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                    startIndex = currentStartIndex;
+                }
+            }
+            
+            for (int i = startIndex; i < startIndex + maxLength; i++)
+            {
+                longestSequence += (numbersArray[i] + " ");
+            }
+            Console.WriteLine(longestSequence);
+        }
+    }
+}
