369a9176ae6133126a5558cad2f169716232fd92
Petar Iliev <peshote@gmail.com>
2023-07-25 23:01:24 +0300

    Upload new problem solved
    P06. Equal Sum

---
+++ 03. Fundamentals-Arrays-(B)Exercise/P06. Equal Sum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace P06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            bool noIndex = true;
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += numbersArray[j];
+                }
+
+                for (int k = i + 1; k < numbersArray.Length; k++)
+                {
+                    rightSum += numbersArray[k];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    noIndex = false;
+                }
+            }
+
+            if (noIndex)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
