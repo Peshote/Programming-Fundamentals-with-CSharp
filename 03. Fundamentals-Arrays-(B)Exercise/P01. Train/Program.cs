dd63045b977500a3476817a9c844d94b822c5235
Petar Iliev <peshote@gmail.com>
2023-07-25 22:55:16 +0300

    Upload new problem solved
    P01. Train

---
+++ 03. Fundamentals-Arrays-(B)Exercise/P01. Train/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonsCount = int.Parse(Console.ReadLine());
+            int[] peopleCountPerVagon = new int[wagonsCount];
+            int totalPassangersCount = 0;
+
+            for (int i = 0; i < peopleCountPerVagon.Length; i++)
+            {
+                peopleCountPerVagon[i] = int.Parse(Console.ReadLine());
+                totalPassangersCount += peopleCountPerVagon[i];
+            }
+
+            Console.WriteLine(string.Join(" ", peopleCountPerVagon));
+            Console.WriteLine(totalPassangersCount);
+        }
+    }
+}
