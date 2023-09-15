46f389e54f0cd96bb0f52b4f4a2811243e1d19a9
Petar Iliev <peshote@gmail.com>
2023-09-15 18:10:44 +0300

    Upload new problem solved
    P10. Multiply Evens by Odds

---
+++ 04. Fundamentals-Methods-(A)Lab/P10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace P10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        private static string input;
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+
+            if (inputNum < 0)
+            {
+                inputNum = Math.Abs(inputNum);
+            }
+
+            string input = inputNum.ToString();
+
+            Console.WriteLine(GetMultipleOfEvenAndOdds());
+        }
+
+        private static int GetMultipleOfEvenAndOdds()
+        {
+            int result = GetSumOfEvenDigits() * GetSumOfOddDigits();
+            return result;
+        }
+
+        private static int GetSumOfEvenDigits()
+        {
+            int result = 0;
+
+            foreach (char digitChar in input)
+            {
+                int digit = int.Parse(digitChar.ToString());
+                if (digit % 2 == 0)
+                {
+                    result += digit;
+                }                
+            }
+            
+            return result;
+        }
+
+        private static int GetSumOfOddDigits()
+        {
+            int result = 0;
+
+            foreach (char digitChar in input)
+            {
+                int digit = int.Parse(digitChar.ToString());
+                if (digit % 2 != 0)
+                {
+                    result += digit;
+                }                
+            }
+
+            return result;
+        }
+    }
+}
