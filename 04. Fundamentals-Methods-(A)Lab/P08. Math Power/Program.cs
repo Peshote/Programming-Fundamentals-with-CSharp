f585fcec7bdb82dd04dd1d95d00deb1a9efc378f
Petar Iliev <peshote@gmail.com>
2023-09-15 18:10:11 +0300

    Upload new problem solved
    P08. Math Power

---
+++ 04. Fundamentals-Methods-(A)Lab/P08. Math Power/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNum = double.Parse(Console.ReadLine());
+            int powerNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(RaiseToPower(baseNum, powerNum));
+        }
+
+        private static double RaiseToPower(double baseNum, int powerNum)
+        {
+            double result = 0d;
+
+            result = Math.Pow(baseNum, powerNum);
+
+            return result;
+        }
+    }
+}
