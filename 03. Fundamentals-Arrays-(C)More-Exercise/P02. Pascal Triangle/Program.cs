ccbe85605c47b22b54374a2d34ed1605ffb5551d
Petar Iliev <peshote@gmail.com>
2023-09-15 18:00:09 +0300

    Upload new problem solved
    P02. Pascal Triangle

---
+++ 03. Fundamentals-Arrays-(C)More-Exercise/P02. Pascal Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P02._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());                   
+
+            uint[] topRow = new uint[1];
+            topRow[0] = 1;
+
+            Console.WriteLine(topRow[0]);
+
+            for (int i = 0; i < n-1; i++)
+            {
+                topRow[0] = 1;
+                topRow[topRow.Length - 1] = 1;
+
+                uint[] currentRow = new uint[topRow.Length + 1];
+                currentRow[0] = 1;
+                currentRow[currentRow.Length - 1] = 1;
+
+                for (int j = 1; j < currentRow.Length - 1; j++)
+                {
+                    currentRow[j] = topRow[j - 1] + topRow[j];
+                }
+                topRow = currentRow;
+
+                Console.WriteLine(string.Join(" ", topRow));
+            }
+        }
+    }
+}
