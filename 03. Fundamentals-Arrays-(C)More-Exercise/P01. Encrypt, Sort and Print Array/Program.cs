411cf5a9498e47c332ed9f0f29e8b4a764119895
Petar Iliev <peshote@gmail.com>
2023-09-15 17:59:48 +0300

    Upload new problem solved
    P01. Encrypt, Sort and Print Array

---
+++ 03. Fundamentals-Arrays-(C)More-Exercise/P01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace P01._Encrypt__Sort_and_Print_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sequenceCount = int.Parse(Console.ReadLine());
+
+            string[] sequences = new string[sequenceCount];
+
+            int[] encryptedSsequences = new int[sequenceCount];
+
+            for (int i = 0; i < sequenceCount; i++)
+            {
+                sequences[i] = Console.ReadLine();
+
+                string currentSequence = sequences[i];
+
+                int currentSum = 0;
+
+                for (int j = 0; j < currentSequence.Length; j++)
+                {
+                    char digit = currentSequence[j];
+                    int value = digit;
+
+                    if (digit == 'a' || digit == 'e' || digit == 'i' || digit == 'o' || digit == 'u' || digit == 'A' || digit == 'E' || digit == 'I' || digit == 'O' || digit == 'U')
+                    {
+                        currentSum += value * currentSequence.Length;
+                    }
+                    else
+                    {
+                        currentSum += value / currentSequence.Length;
+                    }
+                    encryptedSsequences[i] = currentSum;
+                }
+            }
+
+            Array.Sort(encryptedSsequences);
+
+            for (int i = 0;i < encryptedSsequences.Length; i++)
+            {
+                Console.WriteLine(encryptedSsequences[i]);
+            }
+        }
+    }
+}
