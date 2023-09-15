219b8a53004dc3ec755b8d088bc7acb7c2c1ff86
Petar Iliev <peshote@gmail.com>
2023-09-15 18:08:59 +0300

    Upload new problem solved
    P04. Printing Triangle

---
+++ 04. Fundamentals-Methods-(A)Lab/P04. Printing Triangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= input; i++)
+            {
+                PrinTriangleLine(1, i);
+            }
+
+            for (int i = input - 1; i >= 1; i--)
+            {
+                PrinTriangleLine(1, i);
+            }
+        }
+
+        static void PrinTriangleLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
