4b569fed69d7b7f1109ed41785104053c6292ec8
Petar Iliev <peshote@gmail.com>
2023-09-15 18:09:21 +0300

    Upload new problem solved
    P05. Orders

---
+++ 04. Fundamentals-Methods-(A)Lab/P05. Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quan = int.Parse(Console.ReadLine());
+            PrintOrderPrice(product, quan);
+        }
+
+        private static void PrintOrderPrice(string product, int quan)
+        {
+            double price = 0;
+
+            switch (product)
+            {
+                case "coffee":
+                    price = 1.50;
+                    break;
+                case "water":
+                    price = 1.00;
+                    break;
+                case "coke":
+                    price = 1.40;
+                    break;
+                case "snacks":
+                    price = 2.00;
+                    break;
+            }
+
+            Console.WriteLine($"{quan * price:F2}");
+        }
+    }
+}
