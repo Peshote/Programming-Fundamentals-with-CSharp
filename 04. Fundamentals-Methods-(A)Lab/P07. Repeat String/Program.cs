7e31d21574564284ae1db30d82f14e216967de4a
Petar Iliev <peshote@gmail.com>
2023-09-15 18:09:55 +0300

    Upload new problem solved
    P07. Repeat String

---
+++ 04. Fundamentals-Methods-(A)Lab/P07. Repeat String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int repeatCount = int.Parse(Console.ReadLine());
+            Console.WriteLine(RepeatString(input, repeatCount));
+        }
+
+        private static string RepeatString(string input, int repeatCount)
+        {
+            string result = "";
+
+            for (int i = 0; i < repeatCount; i++)
+            {
+                result += input;
+            }
+            return result;
+        }
+    }
+}
