332acccbe1e0cef00775db11ca7119e8ca5ea551
Petar Iliev <peshote@gmail.com>
2023-09-15 18:00:25 +0300

    Upload new problem solved
    P03. Recursive Fibonacci

---
+++ 03. Fundamentals-Arrays-(C)More-Exercise/P03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03._Recursive_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonacciNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SolveMemoization(fibonacciNumber));
+        }
+        public static double SolveMemoization(int fibonacciNumber)
+        {
+            double result;
+            if (memo.ContainsKey(fibonacciNumber))
+            {
+                result = memo[fibonacciNumber];
+            }
+            else
+            {
+                result = SolveMemoization(fibonacciNumber - 1) + SolveMemoization(fibonacciNumber - 2);
+                memo[fibonacciNumber] = result;
+            }
+            return result;
+
+        }
+
+        static Dictionary<int, double> memo = new Dictionary<int, double>() { {1, 1}, {2, 1} };
+    }
+}
