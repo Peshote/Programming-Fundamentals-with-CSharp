ae59e726c20eb58b759dd81cbc86aacfa3f72c48
Petar Iliev <peshote@gmail.com>
2023-09-25 19:20:35 +0300

    Upload new problem solved
    P02. Vowels Count

---
+++ 04. Fundamentals-Methods-(B)Exercise/P02. Vowels Count/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            CountVowels(input);
+        }
+        static void CountVowels(string input)
+        {
+            int vowelCount = 0;
+
+            foreach (char c in input)
+            {                
+                char lowerC = Char.ToLower(c);
+                                
+                if (lowerC == 'a' || lowerC == 'e' || lowerC == 'i' || lowerC == 'o' || lowerC == 'u')
+                {
+                    vowelCount++;
+                }
+            }
+
+            Console.WriteLine(vowelCount);
+        }
+    }
+}
