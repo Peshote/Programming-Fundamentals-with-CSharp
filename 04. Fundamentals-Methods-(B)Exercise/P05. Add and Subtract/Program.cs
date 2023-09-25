232bafa6c0598994c95dc443955ee017e7a632df
Petar Iliev <peshote@gmail.com>
2023-09-25 19:21:21 +0300

    Upload new problem solved
    P05. Add and Subtract

---
+++ 04. Fundamentals-Methods-(B)Exercise/P05. Add and Subtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int sum = CalculateSum(num1, num2);
+            int result = SubtractFromSum(sum, num3);
+                        
+            Console.WriteLine(result);
+        }
+
+        static int CalculateSum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static int SubtractFromSum(int sum, int num3)
+        {
+            return sum - num3;
+        }
+    }
+}
