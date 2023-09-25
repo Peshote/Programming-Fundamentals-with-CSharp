df8a5693c21c723403bfa3af662509e22fdc523a
Petar Iliev <peshote@gmail.com>
2023-09-25 19:23:06 +0300

    Upload new problem solved
    P09. Palindrome Integers

---
+++ 04. Fundamentals-Methods-(B)Exercise/P09. Palindrome Integers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                int number = int.Parse(input);
+                bool isPalindrome = IsPalindrome(number);
+                Console.WriteLine(isPalindrome);
+            }
+        }
+
+        static bool IsPalindrome(int number)
+        {
+            string numberString = number.ToString();
+            int left = 0;
+            int right = numberString.Length - 1;
+
+            while (left < right)
+            {
+                if (numberString[left] != numberString[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
