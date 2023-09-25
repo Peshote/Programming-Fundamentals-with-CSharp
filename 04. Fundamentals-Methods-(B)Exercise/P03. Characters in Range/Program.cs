9012a9e1fa2b8816170ca037cbfbe777cdbcf78f
Petar Iliev <peshote@gmail.com>
2023-09-25 19:20:51 +0300

    Upload new problem solved
    P03. Characters in Range

---
+++ 04. Fundamentals-Methods-(B)Exercise/P03. Characters in Range/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = Console.ReadLine()[0];
+            char secondChar = Console.ReadLine()[0];
+
+            PrintCharactersBetween(firstChar, secondChar);
+        }
+        static void PrintCharactersBetween(char firstChar, char secondChar)
+        {            
+            if (secondChar < firstChar)
+            {
+                char temp = firstChar;
+                firstChar = secondChar;
+                secondChar = temp;
+            }
+            
+            for (char c = (char)(firstChar + 1); c < secondChar; c++)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
