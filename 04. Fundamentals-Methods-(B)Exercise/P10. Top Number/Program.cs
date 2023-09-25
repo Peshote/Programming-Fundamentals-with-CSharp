7ab3dcbd0d039223491930c074ab9a434752648d
Petar Iliev <peshote@gmail.com>
2023-09-25 19:23:19 +0300

    Upload new problem solved
    P10. Top Number

---
+++ 04. Fundamentals-Methods-(B)Exercise/P10. Top Number/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace P10._Top_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintTopNumbers(n);
+        }
+
+        static void PrintTopNumbers(int n)
+        {
+            for (int number = 1; number <= n; number++)
+            {
+                if (IsTopNumber(number))
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+
+        static bool IsTopNumber(int number)
+        {
+            int sumOfDigits = CalculateSumOfDigits(number);
+
+            if (sumOfDigits % 8 != 0)
+            {
+                return false;
+            }
+
+            bool hasOddDigit = ContainsOddDigit(number);
+
+            return hasOddDigit;
+        }
+
+        static int CalculateSumOfDigits(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+
+            return sum;
+        }
+
+        static bool ContainsOddDigit(int number)
+        {
+            while (number != 0)
+            {
+                int digit = number % 10;
+
+                if (digit % 2 != 0)
+                {
+                    return true;
+                }
+
+                number /= 10;
+            }
+
+            return false;
+        }
+    }
+}
