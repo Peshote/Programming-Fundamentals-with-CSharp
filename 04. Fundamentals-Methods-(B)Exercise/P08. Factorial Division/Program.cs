f3a705c8844acd4fe4ebd28857b1884162c83330
Petar Iliev <peshote@gmail.com>
2023-09-25 19:22:52 +0300

    Upload new problem solved
    P08. Factorial Division

---
+++ 04. Fundamentals-Methods-(B)Exercise/P08. Factorial Division/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            double factorial1 = CalculateFactorial(number1);
+            double factorial2 = CalculateFactorial(number2);
+
+            double result = factorial1 / factorial2;
+
+            Console.WriteLine($"{result:F2}");
+        }
+
+        static double CalculateFactorial(int number)
+        {
+            double factorial = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
