c16198e980fb7aab0f799a9fca9db4f9e778e67c
Petar Iliev <peshote@gmail.com>
2023-09-25 19:21:08 +0300

    Upload new problem solved
    P04. Password Validator

---
+++ 04. Fundamentals-Methods-(B)Exercise/P04. Password Validator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace P04._Password_Validator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            bool isValid = IsPasswordValid(password);
+
+            if (isValid)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+
+        public static bool IsPasswordValid(string password)
+        {
+            bool isValid = true;
+
+            if (!IsPasswordLengthValid(password))
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+                isValid = false;
+            }
+
+            if (!IsPasswordConsistOfLettersAndDigits(password))
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+                isValid = false;
+            }
+
+            if (!HasAtLeastTwoDigits(password))
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        public static bool IsPasswordLengthValid(string password)
+        {
+            int passwordLength = password.Length;
+            return passwordLength >= 6 && passwordLength <= 10;
+        }
+
+        public static bool IsPasswordConsistOfLettersAndDigits(string password)
+        {
+            return password.All(char.IsLetterOrDigit);
+        }
+
+        public static bool HasAtLeastTwoDigits(string password)
+        {
+            int digitCount = password.Count(char.IsDigit);
+            return digitCount >= 2;
+        }
+    }
+}
